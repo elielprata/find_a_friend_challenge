54e4d6d2b9d0c655744884d0831afee6db72f09f
Eliel Martins <elieuprata@gmail.com>
2023-06-05 17:45:04 -0300

    create prisma orgs repository

---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,25 @@
+import { Org, Prisma } from '@prisma/client'
+import { OrgsRepository } from '../orgs-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaOrgsRepository implements OrgsRepository {
+  async findById(id: string): Promise<Org | null> {
+    const org = await prisma.org.findUnique({ where: { id } })
+
+    return org
+  }
+
+  async findByEmail(email: string): Promise<Org | null> {
+    const org = await prisma.org.findUnique({ where: { email } })
+
+    return org
+  }
+
+  async create(data: Prisma.OrgCreateInput): Promise<Org> {
+    const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+}
